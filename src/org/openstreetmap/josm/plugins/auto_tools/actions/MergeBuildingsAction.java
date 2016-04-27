fd157eb229723826641e1210f0aa7b25691badc2
Rub21 <ruben@mapbox.com>
2016-04-27 16:17:49 -0500

    merge tags

--- src/org/openstreetmap/josm/plugins/auto_tools/actions/MergeBuildingsAction.java
+++ src/org/openstreetmap/josm/plugins/auto_tools/actions/MergeBuildingsAction.java
@@ -9,20 +9,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.swing.JOptionPane;
-import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JoinAreasAction;
 import org.openstreetmap.josm.actions.JosmAction;
-import org.openstreetmap.josm.command.AddCommand;
 import org.openstreetmap.josm.command.ChangePropertyCommand;
 import org.openstreetmap.josm.command.Command;
 import org.openstreetmap.josm.command.SequenceCommand;
-import org.openstreetmap.josm.data.coor.LatLon;
-import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.OsmPrimitive;
+import org.openstreetmap.josm.data.osm.Tag;
 import org.openstreetmap.josm.data.osm.TagCollection;
 import org.openstreetmap.josm.data.osm.Way;
-import org.openstreetmap.josm.gui.layer.OsmDataLayer;
 import static org.openstreetmap.josm.gui.mappaint.mapcss.ExpressionFactory.Functions.tr;
 import static org.openstreetmap.josm.tools.I18n.trn;
 
@@ -38,51 +34,74 @@ public class MergeBuildingsAction extends JosmAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
         LinkedList<Way> ways = new LinkedList<Way>(Main.main.getCurrentDataSet().getSelectedWays());
-
-        List<LatLon> latLon = new LinkedList<LatLon>();
         if (ways.isEmpty()) {
             JOptionPane.showMessageDialog(null, "Vacio");
         } else {
-            Map atrributes = new HashMap();
+            //Filtrar los Tags, y sacar un promeriod de ellos
+            Map<String, String> atrributes = new HashMap();
             for (OsmPrimitive osm : ways) {
                 Set<String> keys = osm.getKeys().keySet();
                 for (String key : keys) {
                     if (!atrributes.containsKey(key)) {
                         atrributes.put(key, osm.get(key));
                     } else {
-                        String atrr = atrributes.get(key) + ";" + osm.get(key);
-                        atrributes.put(key, atrr);
-                    }
-                }
-                latLon.add(osm.getBBox().getCenter());
-
-            }
-            for (OsmPrimitive osm : ways) {
-                Set<String> keys = osm.getKeys().keySet();
-                for (String key : keys) {
-                    if (atrributes.containsKey(key)) {
-//                        Main.main.undoRedo.add(new ChangePropertyCommand(osm, key, atrributes.get(key).toString()));
-                        Main.main.undoRedo.add(new SequenceCommand(tr("revert tags"), MergeTags(osm, atrributes)));
+                        if (!atrributes.get(key).equals(osm.get(key))) {
+                            String atrr = atrributes.get(key) + ";" + osm.get(key);
+                            atrributes.put(key, atrr);
+                        }
                     }
                 }
             }
+            //Convertir  a tag collections
+            TagCollection tagCollection = new TagCollection();
+            for (Map.Entry<String, String> entry : atrributes.entrySet()) {
+                //https://github.com/osmlab/labuildings/blob/master/IMPORTING.md
+                //lacounty:ain -> ALL
+                //lacount:bld_id -> ALL
+                //start_date -> None if multiple or the one option if there's only one
+                //height -> largest number
+                //ele -> largest number
+                //building:units -> none if different
+                Tag tag;
+                if ((entry.getKey().equals("start_date") || entry.getKey().equals("building:units")) && entry.getValue().contains(";")) {
+                    tag = new Tag(entry.getKey(), null);
+                } else if ((entry.getKey().equals("height") || entry.getKey().equals("ele")) && entry.getValue().contains(";")) {
+                    String[] stringArray = entry.getValue().split(";");
+                    Double max = Double.parseDouble(stringArray[0]);
+                    for (int index = 1; index < stringArray.length; index++) {
+                        Double h = Double.parseDouble(stringArray[index]);
+                        if (h > max) {
+                            max = h;
+                        }
+                    }
+                    tag = new Tag(entry.getKey(), max.toString());
+                } else {
+                    tag = new Tag(entry.getKey(), entry.getValue());
+                }
+                tagCollection.add(tag);
+            }
+            Main.main.undoRedo.add(new SequenceCommand(tr("revert tags"), MergeAllTags(ways, tagCollection)));
             JosmAction build = new JoinAreasAction();
             build.actionPerformed(e);
-//            build.getCurrentDataSet().getSelectedWays();
         }
-
     }
 
-    protected List<Command> MergeTags(OsmPrimitive primitive, Map atrributes) {
-        LinkedList<Command> cmds = new LinkedList<Command>();
-        Set<String> keys = primitive.getKeys().keySet();
-        for (String key : keys) {
-            cmds.add(new ChangePropertyCommand(primitive, key, atrributes.get(key).toString()));
+    protected Command MergeAllTags(Collection<? extends OsmPrimitive> selection, TagCollection tc) {
+        List<Command> commands = new ArrayList<Command>();
+        for (String key : tc.getKeys()) {
+            String value = tc.getValues(key).iterator().next();
+            value = value.equals("") ? null : value;
+            commands.add(new ChangePropertyCommand(selection, key, value));
         }
-        return cmds;
+        if (!commands.isEmpty()) {
+            String title1 = trn("Pasting {0} tag", "Pasting {0} tags", tc.getKeys().size(), tc.getKeys().size());
+            String title2 = trn("to {0} primitive", "to {0} primtives", selection.size(), selection.size());
+            return new SequenceCommand(
+                    title1 + " " + title2,
+                    commands
+            );
+        }
+        return null;
     }
-
-
 }
